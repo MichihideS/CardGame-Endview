79311e49a6bf1486b86c91cc38801369f30231f6
MichihideS <michihides@gmail.com>
2024-10-19 18:14:07 +0200

    add basefile

--- CardGameEndview/CardGameEndviewApp.swift
+++ CardGameEndview/CardGameEndviewApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct CardGameEndviewApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            StartView()
         }
     }
 }
---
+++ CardGameEndview/Model/Card.swift
@@ -0,0 +1,15 @@
+//
+//  Card.swift
+//  CardGameEndview
+//
+//  Created by Michihide Sugito on 2024-10-19.
+//
+
+import Foundation
+
+struct Card {
+    var attack: Int
+    var defense: Int
+    var cost: Int
+    var special: String
+}
