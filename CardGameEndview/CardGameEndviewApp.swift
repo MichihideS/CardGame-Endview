d4cb03b4407874d4c73e5d3ffb9c9b88a559130c
MichihideS <michihides@gmail.com>
2024-10-19 18:06:34 +0200

    Initial Commit

---
+++ CardGameEndview/CardGameEndviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardGameEndviewApp.swift
+//  CardGameEndview
+//
+//  Created by Michihide Sugito on 2024-10-19.
+//
+
+import SwiftUI
+
+@main
+struct CardGameEndviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
